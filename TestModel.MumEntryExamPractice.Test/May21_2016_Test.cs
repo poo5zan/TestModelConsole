186dfe232c96e4e6695b90d9616f979a4d725aff
poo5zan <pujanmaharjan@gmail.com>
2016-06-01 22:58:53 +0545

    added third operation

--- TestModel.MumEntryExamPractice.Test/May21_2016_Test.cs
+++ TestModel.MumEntryExamPractice.Test/May21_2016_Test.cs
@@ -30,5 +30,46 @@ namespace TestModel.MumEntryExamPractice.Test
 
 
         }
+
+        [TestMethod]
+        public void IsMaxMinEqual_should_validate_array() {
+            var may21_2016 = new May21_2016();
+            
+            //Valid 1
+            //Invalid 0
+
+            //Invalid, because array must have at least 
+            //two different elements
+            var result_empty_Array = may21_2016.IsMaxMinEqual(new int[] { });
+            Assert.AreEqual(result_empty_Array, 0);
+            
+            //Invalid, because array must have atleast
+            //two different elements
+            var result_one_array = may21_2016.IsMaxMinEqual(new int[] { 2 });
+            Assert.AreEqual(result_one_array, 0);
+
+            //Invalid, because array must have at least two different elements
+            var result_same_element = may21_2016.IsMaxMinEqual(new int[] { 1, 1, 1, 1, 1, 1 });
+            Assert.AreEqual(result_same_element, 0);
+
+            //Valid, because both max value(11) and 
+            //min value 2 appear exactly one time
+            var result_with_min_max_ocurring_once = may21_2016.IsMaxMinEqual(new int[] { 2, 4, 6, 8, 11 });
+            Assert.AreEqual(result_with_min_max_ocurring_once, 1);
+
+            //valid, because both max and min appears exactly one time
+            var result_with_min_max_negative = may21_2016.IsMaxMinEqual(new int[] { -2, -4, -6, -8, -11 });
+            Assert.AreEqual(result_with_min_max_negative, 1);
+
+            //valid
+            var result = may21_2016.IsMaxMinEqual(new int[] { 11, 4, 9, 11, 8, 5, 4, 10 });
+            Assert.AreEqual(result, 1);
+
+            //invalid
+            var result2 = may21_2016.IsMaxMinEqual(new int[] { 11, 4, 9, 11, 8, 5, 4, 10,11 });
+            Assert.AreEqual(result, 0);
+
+        }
+
     }
 }
--- TestModel.MumEntryExamPractice/May21_2016.cs
+++ TestModel.MumEntryExamPractice/May21_2016.cs
@@ -20,7 +20,7 @@ namespace TestModel.MumEntryExamPractice
 
             //int d = n % p;
             int remainder = 0;
-            
+
             while (remainder == 0)
             {
                 remainder = n % p;
@@ -30,8 +30,57 @@ namespace TestModel.MumEntryExamPractice
                     p = Convert.ToInt32(Math.Pow(p, exponent));
                 }
             }
-            return exponent;            
+            return exponent;
 
         }
+
+        public int IsMaxMinEqual(int[] inputArray)
+        {
+            int _valid = 1;
+            int _invalid = 0;     
+            
+            if (inputArray.Length < 2) {                
+                return 0;
+            }
+            int minimumValue = inputArray[0];
+            int minimumValueCount = 0;
+            int maximumValue = inputArray[0];
+            int maximumValueCount = 0;
+
+            for (int i = 1; i < inputArray.Length; i++)
+            {
+                if (inputArray[i] < minimumValue)
+                {
+                    minimumValue = inputArray[i];
+                }
+                if (inputArray[i] > maximumValue)
+                {
+                    maximumValue = inputArray[i];
+                }
+            }
+
+            //if all the elements of the array is same
+            if (minimumValue == maximumValue) { return 0; }
+
+            //count minimum and maximum value
+            foreach (var element in inputArray)
+            {
+                if (element == minimumValue)
+                {
+                    minimumValueCount++;
+                }
+                if (element == maximumValue)
+                {
+                    maximumValueCount++;
+                }
+            }
+
+            //different # of min and max
+            if (minimumValueCount != maximumValueCount) { return 0; }
+
+            return _valid;
+        }
+
+
     }
 }
