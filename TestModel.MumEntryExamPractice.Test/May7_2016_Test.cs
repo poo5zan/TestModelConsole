4a87e3046838d1c31fb4bdec9e9a1f26c50d78a6
poo5zan <pujanmaharjan@gmail.com>
2016-06-04 14:54:20 +0545

    added may27

---
+++ TestModel.MumEntryExamPractice.Test/May7_2016_Test.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestModel.MumEntryExamPractice.Test
+{
+    [TestClass]
+    public class May7_2016_Test
+    {
+        [TestMethod]
+        public void FactorTwoCount()
+        {
+            var may7_2016 = new May7_2016();
+            var result_48_4 = may7_2016.FactorTwoCount(48);
+            Assert.AreEqual(result_48_4, 4);
+            var result_27_0 = may7_2016.FactorTwoCount(27);
+            Assert.AreEqual(result_27_0, 0);
+        }
+
+        [TestMethod]
+        public void MeeraArray()
+        {
+            var may7_2016 = new May7_2016();
+            //valid
+            var result1 = may7_2016.MeeraArray(new int[] { 4, 8, 6, 3, 2, 9, 8, 11, 8, 13, 12, 12, 6 });
+            Assert.AreEqual(1, result1);
+
+            //invalid
+            var result2 = may7_2016.MeeraArray(new int[] { 2, 4, 6, 8, 6 });
+            Assert.AreEqual(0, result2);
+
+            //invalid
+            var result3 = may7_2016.MeeraArray(new int[] { 2, 8, 7, 10, -4, 6 });
+            Assert.AreEqual(0, result3);
+
+        }
+
+        [TestMethod]
+        public void GoodSpread() {
+            var may7_2016 = new May7_2016();
+
+            var result1 = may7_2016.GoodSpread(new int[] { 2, 1, 2, 5, 2, 1, 5, 9 });
+            Assert.AreEqual(1, result1);
+
+            var result2 = may7_2016.GoodSpread(new int[] { 3, 1, 3, 1, 3, 5, 5, 3 });
+            Assert.AreEqual(0, result2);
+                        
+        }
+    }
+}
---
+++ TestModel.MumEntryExamPractice/May7_2016.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestModel.MumEntryExamPractice
+{    
+    public class May7_2016
+    {
+        public int FactorTwoCount(int number)
+        {
+            int remainder = 0;
+            int countNumber = 0;
+            while (remainder == 0)
+            {
+                remainder = number % 2;
+                if (remainder == 0)
+                {
+                    number = number / 2;
+                    countNumber++;
+                    
+                }
+            }
+            return countNumber;
+        }
+
+        public int MeeraArray(int[] inputArray)
+        {
+            int valid = 1;
+            int invalid = 0;
+            int oddNumber = 0;
+            //check if inputArray contains odd number
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                if (inputArray[i] % 2 == 1)
+                {
+                    oddNumber = inputArray[i];
+                    break;
+                }
+            }
+
+            //if no odd number found, return invalid
+            if (oddNumber == 0) {
+                return invalid;
+            }
+
+            //count if number of consecutive even numbers matches odd number
+            for (int i = 0, j = inputArray.Length - 1; i < oddNumber; i++)
+            {
+                //check if the number is odd
+                if ((inputArray[i] % 2 == 1) || (inputArray[j] % 2 == 1))
+                {
+                    return invalid;
+                }
+            }
+
+            return valid;
+        }
+
+        public int GoodSpread(int[] inputArray) {
+            int Valid = 1;
+            int InValid = 0;
+
+            //limited elements to check
+            if (inputArray.Length < 4) {
+                return Valid;
+            }
+
+            for (int i = 0, j = 1, k = 2, l = 3; i < inputArray.Length; i++)
+            {
+                if
+            }
+
+
+            return Valid;
+        }
+    }
+}
