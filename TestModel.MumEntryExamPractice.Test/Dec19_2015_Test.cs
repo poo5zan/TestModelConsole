1c4f9d1253a7ca491cee302c490f4aab26dfb1c4
poo5zan <pujanmaharjan@gmail.com>
2016-06-10 12:55:09 +0545

    added Dec 19 solution of MUM Entry Exam

---
+++ TestModel.MumEntryExamPractice.Test/Dec19_2015_Test.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestModel.MumEntryExamPractice.Test
+{
+    [TestClass]
+    public class Dec19_2015_Test
+    {
+        [TestMethod]
+        public void IsMeera()
+        {
+            var dec19_2015 = new Dec19_2015();
+
+            var result6 = dec19_2015.IsMeera(6);
+            Assert.AreEqual(1, result6);
+
+            var result30 = dec19_2015.IsMeera(30);
+            Assert.AreEqual(1, result30);
+
+            var result21 = dec19_2015.IsMeera(21);
+            Assert.AreEqual(0, result21);
+
+        }
+
+        [TestMethod]
+        public void IsBunker() {
+            var dec19_2016 = new Dec19_2015();
+
+            var result1 = dec19_2016.IsBunker(new int[] { 7, 6, 10, 1 });
+            Assert.AreEqual(1,result1);
+
+            var result2 = dec19_2016.IsBunker(new int[] { 7, 6, 10 });
+            Assert.AreEqual(0,result2);
+
+            var result3 = dec19_2016.IsBunker(new int[] { 6, 10, 1 });
+            Assert.AreEqual(0,result3);
+
+            var result4 = dec19_2016.IsBunker(new int[] { 3, 7, 1, 8, 1 });
+            Assert.AreEqual(1,result4);
+
+           
+        }
+
+        [TestMethod]
+        public void IsNice() {
+
+            var dec19_2015 = new Dec19_2015();
+
+            var result1 = dec19_2015.IsNice(new int[] { 2, 10, 9, 3 });
+            Assert.AreEqual(1,result1);
+
+            var result2 = dec19_2015.IsNice(new int[] { 2, 2, 3, 3, 3 });
+            Assert.AreEqual(1,result2);
+
+            var result3 = dec19_2015.IsNice(new int[] { 1, 1, 1, 2, 1, 1 });
+            Assert.AreEqual(1,result3);
+
+            var result4 = dec19_2015.IsNice(new int[] { 0, -1, 1 });
+            Assert.AreEqual(1,result4);
+
+            var result5 = dec19_2015.IsNice(new int[] { 3, 4, 5, 7 });
+            Assert.AreEqual(0,result5);
+        }
+
+    }
+}
---
+++ TestModel.MumEntryExamPractice/Dec19_2015.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestModel.MumEntryExamPractice
+{
+    public class Dec19_2015
+    {
+        public int IsMeera(int number) {
+            int remainder = 0;
+            int count = 0;
+
+            for (int i = 2; i < number; i++)
+            {
+                remainder = number % i;
+                if (remainder == 0)
+                {
+                    count++;
+                }
+            }
+
+            if (number % count == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int IsBunker(int[] inputArray)
+        {
+            bool foundOne = false;
+            bool foundPrime = false;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                //skipping checking prime, if already found
+                if (foundPrime == false)
+                {
+                    if (Helpers.IsPrime(inputArray[i]) == 1)
+                    {
+                        foundPrime = true;
+                    }
+                }
+                //check one
+                if (foundOne == false)
+                {
+                    if (inputArray[i] == 1) { foundOne = true; }
+                }
+            }
+
+            if (foundOne == true && foundPrime == true)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int IsNice(int[] inputArray) {
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                bool foundElement = false;
+                for (int j = 0; j < inputArray.Length; j++)
+                {
+                    if (inputArray[i] == inputArray[j] - 1
+                      || inputArray[i] == inputArray[j] + 1)
+                    {
+                        foundElement = true;
+                        break;
+                    }
+                }
+
+                if (foundElement == false) {
+                    return 0;
+                }
+            }
+            return 1;
+        }
+    }
+}
