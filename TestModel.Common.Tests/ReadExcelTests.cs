02db5961a3fe79dd4d0e6fd10efe7c04f1546c63
PujanMaharjan <pujanmaharjan@gmail.com>
2018-01-09 16:45:29 +0545

    Read Excel file and SqlBulkCopy Test

---
+++ TestModel.Common.Tests/ReadExcelTests.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestModel.Common.Tests
+{
+    [TestClass]
+    public class ReadExcelTests
+    {
+        [TestMethod]
+        public void ReadExcel()
+        {
+            string filePath = @"D:\readExcel\sample_excel.xlsx";
+            var dt = ReadExcelToDataTable(filePath);
+            var columns = dt.Columns;
+        }
+
+        private DataTable ReadExcelToDataTable(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new Exception("File Not Found");
+            }
+            FileInfo fileInfo = new FileInfo(filePath);
+            XSSFWorkbook xssfWorkBook = null;
+            HSSFWorkbook hssfWorkBook = null;
+            int sheetIndex = 0;
+            ISheet sheet = null;
+
+            using (FileStream file = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                if (fileInfo.Extension == ".xlsx")
+                {
+                    xssfWorkBook = new XSSFWorkbook(file);
+                    sheetIndex = xssfWorkBook.ActiveSheetIndex;
+                    sheet = xssfWorkBook.GetSheetAt(sheetIndex);                    
+                }
+                else if (fileInfo.Extension == ".xls")
+                {
+                    hssfWorkBook = new HSSFWorkbook(file);
+                    sheetIndex = hssfWorkBook.ActiveSheetIndex;
+                    sheet = hssfWorkBook.GetSheetAt(sheetIndex);
+                }
+                else
+                {
+                    throw new Exception("Not a valid Excel extension. Supported Extensions are xls and xlsx");
+                }
+            }
+           
+            return ConvertExcelSheetToDataTable(sheet);
+        }
+
+        private DataTable ConvertExcelSheetToDataTable(ISheet sheet)
+        {
+            if (sheet == null)
+            {
+                return null;
+            }
+
+            DataTable dt = new DataTable();
+            for (int rowIndex = sheet.FirstRowNum; rowIndex <= sheet.LastRowNum; rowIndex++)
+            {
+                //the first row found is treated as header
+                if (rowIndex == sheet.FirstRowNum)
+                {
+                    IRow headerRow = sheet.GetRow(rowIndex);
+                    if (headerRow != null)
+                    {
+                        for (int columnIndex = headerRow.FirstCellNum; columnIndex < headerRow.LastCellNum; columnIndex++)
+                        {
+                            string headerName = headerRow.GetCell(columnIndex)?.StringCellValue;
+                            dt.Columns.Add(headerName);
+                        }
+                    }
+                }
+
+                IRow row = sheet.GetRow(rowIndex);
+                if (row != null)
+                {
+                    DataRow dtRow = dt.NewRow();
+                    int dtRowIndex = 0;
+                    for (int columnIndex = row.FirstCellNum; columnIndex < row.LastCellNum; columnIndex++)
+                    {
+                        string columnValue = row.GetCell(columnIndex)?.StringCellValue;
+                        dtRow[dtRowIndex] = columnValue;
+                        dtRowIndex++;
+                    }
+                    dt.Rows.Add(dtRow);
+                }
+            }
+            return dt;
+        }
+                
+    }
+}
---
+++ TestModelConsole/SqlBulkCopyTrial.cs
@@ -0,0 +1,69 @@
+﻿using LumenWorks.Framework.IO.Csv;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestModelConsole
+{
+    public class SqlBulkCopyTrial
+    {
+        public void SqlBulkCopyToDumpCsvToTable()
+        {
+            Console.WriteLine("Starting");
+            string filename = @"\\langtang\UDH\puj\pimproduct_sep_5_2017.csv";
+            string connectionString = @"Data Source=langtang\sql12; Initial Catalog= DEV_GBD_UDH_REARCH; Integrated Security=False; User ID=dh_admin;Password=GBD@dh_ad; MultipleActiveResultSets=True;";
+
+            //SqlConnection conn = new SqlConnection(connectionString);
+            // conn.Open();
+
+            //SqlTransaction transaction = conn.BeginTransaction();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+                    using (StreamReader file = new StreamReader(filename))
+                    {
+                        CsvReader csv = new CsvReader(file, true, ',');
+
+                        using (SqlBulkCopy copy = new SqlBulkCopy(conn))
+                        {
+                            //, SqlBulkCopyOptions.KeepIdentity, transaction);
+                            copy.DestinationTableName = "pujan";
+                            copy.BulkCopyTimeout = 0;
+                            //copy.NotifyAfter = 2000;
+                            //copy.SqlRowsCopied += Copy_SqlRowsCopied;                            
+                            Console.WriteLine("Bulkcopy started = " + DateTime.Now);
+                            copy.BatchSize = 2000;
+                            copy.WriteToServer(csv);
+                            //transaction.Commit();
+                            Console.WriteLine("Bulkcopy ended = " + DateTime.Now);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                // transaction.Rollback();
+            }
+            finally
+            {
+                //conn.Close();
+            }
+        }
+
+        private void Copy_SqlRowsCopied(object sender, SqlRowsCopiedEventArgs e)
+        {
+            Console.WriteLine(sender);
+            Console.WriteLine("Rows copied = " + e.RowsCopied + ", time = " + DateTime.Now);
+            //throw new NotImplementedException();
+        }
+    }
+}
+
