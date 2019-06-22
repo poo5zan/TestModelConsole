eace90dcb5bd9e9afa383c92f685da1c4f0efe1b
Pujan <pujanmaharjan@gmail.com>
2019-06-22 11:14:05 +0545

    random

---
+++ BaseMicroService/IBaseMicroService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseMicroService
+{
+    public interface IBaseMicroService
+    {
+        void Start(HostControl hostControl);
+        void Stop();
+        void Pause();
+        void Continue();
+        void Shutdown();
+        void Resume();
+        void TryRequest(Action action, int maxFailures, int startTimeoutMS = 100,
+            int resetTimeout = 10000, Action<Exception> onError = null);
+    }
+}
